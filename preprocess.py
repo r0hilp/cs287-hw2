8f8b976315650b3701d3ecb7119105717eb73035
Jeffrey Ling <jeffrey.r.ling@gmail.com>
2016-02-18 01:42:48 -0500

    preproc working, have not trained yet

--- preprocess.py
+++ preprocess.py
@@ -40,45 +40,64 @@ def get_tag_ids(tag_dict):
 
 def convert_data(data_name, word_to_idx, tag_to_id, window_size, common_words_list, dataset):
     # Construct index/capital feature sets for each file
+    V = len(word_to_idx) + 1
+
     features = []
+    cap_features = []
     lbl = []
     window_features = []
+    window_cap_features = []
     window_lbl = []
     with codecs.open(data_name, "r", encoding="latin-1") as f:
         # initial padding
-        for _ in range(window_size/2):
-            features.append([1, 0, 0, 0, 0])
-            lbl.append(0)
+        features.extend([1] * (window_size/2))
+        cap_features.extend([1] * (window_size/2))
+        lbl.extend([0] * (window_size/2))
+        
         for line in f:
             line = line.rstrip()
             if len(line) == 0:
                 # add padding word
-                features.append([1, 0, 0, 0, 0])
-                lbl.append(0)
+                features.extend([1] * (window_size/2))
+                cap_features.extend([1] * (window_size/2))
+                lbl.extend([0] * (window_size/2))
             else:
-                global_id, _, word, tag = tuple(line.split('\t'))
+                _, _, word, tag = tuple(line.split('\t'))
                 lower_caps = int(word.islower())
                 all_caps = int(word.isupper())
                 first_letter_cap = int(word[0].isupper())
                 has_one_cap = int(any(let.isupper() for let in word))
+                if lower_caps:
+                    cap = V+1
+                elif all_caps:
+                    cap = V+2
+                elif first_letter_cap:
+                    cap = V+3
+                elif has_one_cap:
+                    cap = V+4
                 word = clean_str(word, common_words_list)
-                features.append([word_to_idx[word], lower_caps, all_caps, first_letter_cap, has_one_cap])
+
+                features.append(word_to_idx[word])
+                cap_features.append(cap)
                 lbl.append(tag_to_id[tag])
         # end padding
-        for _ in range(window_size/2):
-            features.append([1, 0, 0, 0, 0])
-            lbl.append(0)
+        features.extend([1] * (window_size/2))
+        cap_features.extend([1] * (window_size/2))
+        lbl.extend([0] * (window_size/2))
+
     # Convert to windowed features
     for i in range(len(features)):
         # Skip padding
-        if features[i][0] == 1:
+        if features[i] == 1:
             continue
         else:
-            window_idxs = range(i - window_size/2, i + window_size/2 + 1)
-            window = sum([features[j] for j in window_idxs], [])
-            window_features.append(window)
+            # window_idxs = range(i - window_size/2, i + window_size/2 + 1)
+            i_low = i - window_size/2
+            i_high = i + window_size/2 + 1
+            window_features.append(features[i_low:i_high])
+            window_cap_features.append(cap_features[i_low:i_high])
             window_lbl.append(lbl[i])
-    return np.array(window_features, dtype=np.int32), np.array(window_lbl, dtype=np.int32)
+    return np.array(window_features, dtype=np.int32), np.array(window_cap_features, dtype=np.int32), np.array(window_lbl, dtype=np.int32)
 
 def get_vocab(file_list, common_words_list, dataset=''):
     # Construct index feature dictionary.
@@ -99,11 +118,26 @@ def get_vocab(file_list, common_words_list, dataset=''):
                         idx += 1
     return word_to_idx
 
+def load_word_vecs(file_name, vocab):
+    # Get word vecs from glove
+    word_vecs = {}
+    with codecs.open(file_name, "r", encoding="latin-1") as f:
+        for line in f:
+            vals = line.split()
+            word = vals[0]
+            vals = vals[1:]
+            if word in vocab:
+                word_vecs[word] = vals
+
+    return word_vecs
+
 
 FILE_PATHS = {"PTB": ("data/train.tags.txt",
                       "data/dev.tags.txt",
-                      "data/test.tags.txt",
-                      "data/tags.dict")}
+                      "data/test.tags.txt"),
+              "test": ("data/test_file.txt","data/test_file.txt","data/test_file.txt")}
+TAG_DICT = "data/tags.dict"
+WORD_VECS_PATH = 'data/glove.6B.50d.txt'
 args = {}
 
 
@@ -116,34 +150,57 @@ def main(arguments):
                         type=str)
     args = parser.parse_args(arguments)
     dataset = args.dataset
-    train, valid, test, tag_dict = FILE_PATHS[dataset]
+    train, valid, test = FILE_PATHS[dataset]
 
     # Fix this for now
     window_size = 5
 
     # Retrieve tag to id mapping
-    tag_to_id = get_tag_ids(tag_dict)
+    print 'Get tag ids...'
+    tag_to_id = get_tag_ids(TAG_DICT)
 
     # Retrieve common words
-    common_words_list = get_common_words('data/glove.6B.50d.txt')
+    print 'Getting common words...'
+    common_words_list = get_common_words(WORD_VECS_PATH)
 
     # Get index features
+    print 'Getting vocab...'
     word_to_idx = get_vocab([train, valid, test], common_words_list, dataset)
 
     # Convert data
-    train_input, train_output = convert_data(train, word_to_idx, tag_to_id, window_size, common_words_list, dataset)
+    print 'Processing data...'
+    train_input, train_cap_input, train_output = convert_data(train, word_to_idx, tag_to_id, window_size, common_words_list, dataset)
 
     if valid:
-        valid_input, valid_output = convert_data(valid, word_to_idx, tag_to_id, window_size, common_words_list, dataset)
+        valid_input, valid_cap_input, valid_output = convert_data(valid, word_to_idx, tag_to_id, window_size, common_words_list, dataset)
 
     if test:
-        test_input, _ = convert_data(test, word_to_idx, tag_to_id, window_size, common_words_list, dataset)
+        test_input, test_cap_input, _ = convert_data(test, word_to_idx, tag_to_id, window_size, common_words_list, dataset)
 
+    # +4 for cap features
     V = len(word_to_idx) + 1 + 4
-    print('Vocab size:', V - 4)
+    print('Vocab size:', V)
 
-    C = np.max(train_output)
+    train_input = np.hstack((train_input, train_cap_input))
+    valid_input = np.hstack((valid_input, valid_cap_input))
+    test_input = np.hstack((test_input, test_cap_input))
 
+    C = len(tag_to_id)
+
+    # Get word vecs
+    print 'Getting word vecs...'
+    word_vecs = load_word_vecs(WORD_VECS_PATH, word_to_idx)
+    embed = np.random.uniform(-0.25, 0.25, (V, len(word_vecs.values()[0])))
+    # zero out padding
+    embed[0] = 0
+    for word, vec in word_vecs.items():
+        embed[word_to_idx[word] - 1] = vec
+
+    print train_input, train_input.shape
+    print V, C
+    print embed, embed.shape
+
+    print 'Saving...'
     filename = args.dataset + '.hdf5'
     with h5py.File(filename, "w") as f:
         f['train_input'] = train_input
@@ -155,6 +212,8 @@ def main(arguments):
             f['test_input'] = test_input
         f['nfeatures'] = np.array([V], dtype=np.int32)
         f['nclasses'] = np.array([C], dtype=np.int32)
+
+        f['word_vecs'] = embed
 
 
 if __name__ == '__main__':
